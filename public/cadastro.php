2585b03fb7af735a6a8328030affa5f9b122a3b4
Thiago Paes <mrprompt@gmail.com>
2015-05-04 21:59:38 +0000

    cors

--- public/cadastro.php
+++ public/cadastro.php
@@ -18,6 +18,12 @@
  * @version    $Revision: 0.1 $
  * @license    http://www.opensource.org/licenses/mit-license.php The MIT License
  */
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true ");
+header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
+header("Content-type:application/json");
+
 /**
  * @see Usuarios
  */
@@ -64,10 +70,4 @@ try {
     $retorno = $e->getMessage();
 }
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Credentials: true ");
-header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
-header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
-header('Content-type:application/json');
 echo json_encode($retorno);
-
--- public/login.php
+++ public/login.php
@@ -18,18 +18,18 @@
  * @version    $Revision: 0.1 $
  * @license    http://www.opensource.org/licenses/mit-license.php The MIT License
  */
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true ");
+header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
+header("Content-type:application/json");
+
 /**
  * @see Usuarios
  */
 require_once __DIR__ . '/../src/Usuarios.php';
 
 $retorno = null;
-
-// se não for um ajax já paro
-if (empty($_SERVER['HTTP_X_REQUESTED_WITH'])
-        || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) !== 'xmlhttprequest') {
-    die();
-}
 
 try {
     $objUsuarios = new \Usuarios();
@@ -41,9 +41,4 @@ try {
     $retorno = $e->getMessage();
 }
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Credentials: true ");
-header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
-header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
-header('Content-type:application/json');
 echo json_encode($retorno);
